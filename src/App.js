653e779362293fbea1941fa8eec6b8be1c6860f6
donny <donny.pranata@dpranata.com>
2023-07-05 15:29:52 +0700

    feat: add spinner

--- src/App.js
+++ src/App.js
@@ -34,7 +34,7 @@ const App = () => {
   }, []);
 
   return (
-    <Suspense fallback={Spinner}>
+    <Suspense fallback={<Spinner />}>
       <Routes>
         <Route path="/" element={<Navigation />}>
           <Route index element={<Home />} />
